f8c189636a78403d989dcdbec35ca4494edd7f5d
Scott Roberts <sroberts@github.com>
2014-08-01 12:14:19 -0400

    a few basics

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ utilitybelt
 ===========
 
 A Python library for being a CND Batman....
+
+## Setup
+You'll want to run ```script/bootstrap``` to install the necessary dependencies (under VirtualEnv) and to pull down the GeoLiteCity database from [Maxmind](https://www.maxmind.com/en/home).
---
+++ utilitybelt.py
@@ -0,0 +1,92 @@
+import GeoIP
+import json
+
+gi = GeoIP.open("./data/GeoLiteCity.dat", GeoIP.GEOIP_STANDARD)
+
+def is_IPv4Address(ipv4address):
+    pass
+
+def ip_to_geo(ipaddress):
+    """Convert IP to Geographic Information"""
+
+    gir = gi.record_by_addr(ipaddress)
+
+    geo = {
+        "city": gir["city"],
+        "region": gir["region_name"],
+        "country": gir["country_name"],
+        "latitude": gir["latitude"],
+        "longitude": gir["longitude"]
+    }
+
+    return geo
+
+def domain_to_geo(domain):
+    """Convert Domain to Geographic Information"""
+
+    gir = gi.record_by_name(domain)
+
+    geo = {
+        "city": gir["city"],
+        "region": gir["region_name"],
+        "country": gir["country_name"],
+        "latitude": gir["latitude"],
+        "longitude": gir["longitude"]
+    }
+
+    return json.dumps(geo)
+
+def ip_to_geojson(ipaddress, name="Point"):
+    """Generate GeoJSON for given IP address"""
+
+    geo = ip_to_geo(ipaddress)
+
+    point = {
+        "type": "FeatureCollection",
+        "features": [
+            {
+                "type": "Feature",
+                "properties": {
+                    "name": name
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [
+                        geo["longitude"],
+                        geo["latitude"]
+                    ]
+                }
+            }
+        ]
+    }
+
+    return json.dumps(point)
+
+def ips_to_geojson(ipaddresses):
+    """Generate GeoJSON for given IP address"""
+
+    features = []
+
+    for ipaddress in ipaddresses:
+        geo = ip_to_geo(ipaddress)
+
+        features.append({
+            "type": "Feature",
+            "properties": {
+                "name": ipaddress
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    geo["longitude"],
+                    geo["latitude"]
+                ]
+            }
+        })
+
+    points = {
+        "type": "FeatureCollection",
+        "features": features
+    }
+
+    return json.dumps(points)
