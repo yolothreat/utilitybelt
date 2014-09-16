59eca5396f87e8e8d4a012acea97ba1d1bc43f02
Kyle Maxwell <kmaxwell@verisign.com>
2014-09-15 19:05:51 -0500

    Add regexes and simplify logic slightly

--- utilitybelt.py
+++ utilitybelt.py
@@ -17,33 +17,49 @@ import json, re, socket
 
 gi = GeoIP.open("./data/GeoLiteCity.dat", GeoIP.GEOIP_STANDARD)
 
+# Indicators
+re_ipv4 = re.compile("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", re.I | re.S | re.M)
+re_email = re.compile("\\b[A-Za-z0-9_.]+@[0-9a-z.-]+\\b", re.I | re.S | re.M)
+re_domain = re.compile("([a-z0-9-_]+\\.){1,4}(com|aero|am|asia|au|az|biz|br|ca|cat|cc|ch|co|coop|cx|de|edu|fr|gov|hk|info|int|ir|jobs|jp|kr|kz|me|mil|mobi|museum|name|net|nl|nr|org|post|pre|ru|tel|tk|travel|tw|ua|uk|uz|ws|xxx)", re.I | re.S | re.M)
+re_cve = re.compile("(CVE-(19|20)\\d{2}-\\d{4})", re.I | re.S | re.M)
+
+# Hashes
+re_md5 = re.compile("\\b[a-f0-9]{32}\\b", re.I | re.S | re.M)
+re_sha1 = re.compile("\\b[a-f0-9]{40}\\b", re.I | re.S | re.M)
+re_sha256 = re.compile("\\b[a-f0-9]{64}\\b", re.I | re.S | re.M)
+re_sha512 = re.compile("\\b[a-f0-9]{128}\\b", re.I | re.S | re.M)
+re_ssdeep = re.compile("\\b\\d{2}:[A-Za-z0-9/+]{3,}:[A-Za-z0-9/+]{3,}\\b", re.I | re.S | re.M)
+
+# File Types
+re_doc = '\W([\w-]+\.)(docx|doc|csv|pdf|xlsx|xls|rtf|txt|pptx|ppt)'
+re_web = '\W([\w-]+\.)(html|php|js)'
+re_exe = '\W([\w-]+\.)(exe|dll|jar)'
+re_zip = '\W([\w-]+\.)(zip|zipx|7z|rar|tar|gz)'
+re_img = '\W([\w-]+\.)(jpeg|jpg|gif|png|tiff|bmp)'
+re_flash = '\W([\w-]+\.)(flv|swf)'
+
 def is_IPv4Address(ipv4address):
     """Returns true for valid IPv4 Addresses, false for invalid."""
 
-    ip_regex = '(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)'
-    if re.match(ip_regex, ipv4address):
-        return True
-    else:
-        return False
+    return re.match(re_ipv4, ipv4address):
+
 
 def ip_to_geo(ipaddress):
     """Convert IP to Geographic Information"""
 
-    geo = gi.record_by_addr(ipaddress)
+    return gi.record_by_addr(ipaddress)
 
-    return geo
 
 def domain_to_geo(domain):
     """Convert Domain to Geographic Information"""
 
-    geo = gi.record_by_name(domain)
+    return gi.record_by_name(domain)
 
-    return geo
 
 def ip_to_geojson(ipaddress, name="Point"):
     """Generate GeoJSON for given IP address"""
 
-    geo = gi.record_by_addr(ipaddress)
+    geo = ip_to_geo(ipaddress)
 
     point = {
         "type": "FeatureCollection",
@@ -95,6 +111,7 @@ def ips_to_geojson(ipaddresses):
 
     return points
 
+
 def reverse_dns_sna(ipaddress):
     """Returns a list of the dns names that point to a given ipaddress using StatDNS API"""
 
@@ -111,6 +128,7 @@ def reverse_dns_sna(ipaddress):
     else:
         raise Exception("No PTR record for %s" % ipaddress)
         return ""
+
 
 def reverse_dns(ipaddress):
     """Returns a list of the dns names that point to a given ipaddress"""
