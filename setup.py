1e9ee0bf3613d395d4513e6dd67ea15c73b6f268
Kyle Maxwell <krmaxwell@gmail.com>
2015-04-17 02:00:31 +0000

    Move requirements into setup.py and fix requirements conflict

--- setup.py
+++ setup.py
@@ -9,5 +9,8 @@ setup(
     license="MIT",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=['requests', 'GeoIP']
+    install_requires=['requests>=2.6.0', 
+                      'pygeoip==0.3.2',
+                      'pytest>=2.6.0'
+                     ]
 )
