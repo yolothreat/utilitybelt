48bf7d2af70a4f177778e43b043dfc557b9c5e15
Kyle Maxwell <krmaxwell@gmail.com>
2015-04-17 02:16:14 +0000

    Use distutils instead of setuptools and specify package

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from distutils.core import setup
 
 
 setup(
@@ -7,11 +7,11 @@ setup(
     description="Utilities to make you a CND Batman",
     url="https://github.com/yolothreat/utilitybelt",
     license="MIT",
-    packages=find_packages(),
     include_package_data=True,
     install_requires=['requests>=2.6.0', 
                       'pygeoip==0.3.2',
                       'pytest>=2.6.0'
                      ],
     package_dir={'utilitybelt': 'src'},
+    packages=['utilitybelt']
 )
